5a15e253f6ce1b0aa0533f79129a519b1f086fd0
Ravinder Soni <sonirav@gmail.com>
2017-05-20 13:55:37 -0700

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+ INSERT INTO burgers SET burger_name = 'Vegi Delight', devoured = 0;
+ INSERT INTO burgers SET burger_name = 'Big Mac', devoured = 1;
+ INSERT INTO burgers SET burger_name = 'Double Meat', devoured = 0;
+ INSERT INTO burgers SET burger_name = 'Chicken Double', devoured = 1;
